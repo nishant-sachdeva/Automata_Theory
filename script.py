2f0befe898bff5c2ea2e75d45b45b7e2eac49df2
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2019-08-17 20:31:30 +0530

    version 1 complete

--- script.py
+++ script.py
@@ -25,7 +25,72 @@ output_start = input_start
 
 # output_function banana padhega , and output states bhi,
 
-for state in range(0,output_number_of_states):
-    # we will make the dfa for this state now,
-     
-    
+output_states = []
+output_final = []
+
+for i in range (0, pow(2, input_number_of_states)):
+    # now we will run that exponential thing
+    state = []
+    for j in range(0, input_number_of_states):
+        if (i & (1<<j)):
+            # then that means that weight in valid
+            state.append(j)
+    output_states.append(state)
+    for j in state:
+        if j in input_final:
+            output_final.append(state)
+            
+
+
+# we have all the output states now, 
+# presently we will start forming the output values
+
+modified_input = {}
+
+for ele in input_function:
+    a = str(ele[0])
+    b = ele[1]
+    c = a + b
+    modified_input[c] = ele[2]
+
+
+# print(modified_input)
+output_function = []
+
+for state in output_states:
+    for letter in input_alphabet:
+        # I have to form a list that will have the current state
+        cur = []
+        cur.append(state)
+        cur.append(letter)
+        codomain = []
+        # that gives me the elements, now I have to form the output
+        for s in state:
+            c = str(s) + str(letter)
+            if c in modified_input:
+                codomain.extend(modified_input[c])
+        set(codomain)
+        list(codomain)
+        cur.append(codomain)
+
+        output_function.append(cur)
+
+
+for output in output_function:
+    print(output)
+
+
+output = {}
+
+output['states'] = pow(2, input_number_of_states)
+output['letters'] = input_alphabet
+output['t_func'] = output_function
+output['start'] = 0
+output['final'] = output_final
+
+with open('output.json', 'w') as outfile:
+    json.dump(output, outfile)
+
+
+
+
